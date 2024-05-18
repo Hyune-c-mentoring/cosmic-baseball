3a67863e8eb3cfe63f2f6d8ef1bdbb2b769ee4a2
mingMy-00 <wbsebqb1118@naver.com>
2024-05-18 13:58:38 +0900

    [1LV 일반야구게임] 게임구현, 테스트코드 구현
    [1LV 일반야구게임] 테스트 코드 구현

--- src/test/java/com/hyunec/cosmicbaseball/acceptancetest/CosmicBaseballLv1Test.java
+++ src/test/java/com/hyunec/cosmicbaseball/acceptancetest/CosmicBaseballLv1Test.java
@@ -2,11 +2,20 @@ package com.hyunec.cosmicbaseball.acceptancetest;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
 class CosmicBaseballLv1Test {
-    @DisplayName("타격 결과는 모두 같은 확률을 가집니다.")
+    @DisplayName("타격 결과는 모두 같은 확률을 가집니다.") //테스트의 목적을 설명하는 어노테이션
     @Test
-    void t1() {
+    void t1()  {
         throw new RuntimeException("Not yet implemented");
     }
 
@@ -15,4 +24,6 @@ class CosmicBaseballLv1Test {
     void t2() {
         throw new RuntimeException("Not yet implemented");
     }
+
+    // 아직 구현이 안돼서 RuntimeException을 날림
 }
