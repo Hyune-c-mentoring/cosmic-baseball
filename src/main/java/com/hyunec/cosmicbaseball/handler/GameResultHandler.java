c4a9559def141b16e4ed1318702f14eeb4f7e650
mingMy-00 <wbsebqb1118@naver.com>
2024-05-19 19:28:59 +0900

    [Level 1 일반야구게임] - pr리뷰 업데이트

---
+++ src/main/java/com/hyunec/cosmicbaseball/handler/GameResultHandler.java
@@ -0,0 +1,9 @@
+package com.hyunec.cosmicbaseball.handler;
+
+import org.springframework.stereotype.Component;
+
+public interface GameResultHandler {
+    String handleResult(String result);
+
+    String handleOnceResult(String result);
+}
